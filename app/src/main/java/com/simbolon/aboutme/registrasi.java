052b901e2e6af72890d29f7bbe586652e5b47fd2
BoySimbolon <2281040@unai.edu>
2024-01-31 14:34:17 +0700

    version 1.5

--- app/src/main/java/com/simbolon/aboutme/registrasi.java
+++ app/src/main/java/com/simbolon/aboutme/registrasi.java
@@ -1,6 +1,9 @@
 package com.simbolon.aboutme;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -12,41 +15,69 @@ import com.simbolon.aboutme.R;
 
 public class registrasi extends AppCompatActivity {
 
-    private EditText konPwdEditText, nDpnEditText, nBlkngEditText, nIMEditText, thnAngkatanEditText, noMknEditText, pwdEditText;
-    private Button btnRegis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrasi);
-
-        konPwdEditText = findViewById(R.id.kon_pwd);
-        nDpnEditText = findViewById(R.id.n_dpn);
-        nBlkngEditText = findViewById(R.id.n_blkng);
-        nIMEditText = findViewById(R.id.NIM);
-        thnAngkatanEditText = findViewById(R.id.thn_angkatan);
-        noMknEditText = findViewById(R.id.no_mkn);
-        pwdEditText = findViewById(R.id.pwd);
-
-        btnRegis = findViewById(R.id.btn_regis);
-        btnRegis.setOnClickListener(new View.OnClickListener() {
+//     mendata setiap objek yang kita akan digunakan
+        EditText txtn_dpn = (EditText) findViewById(R.id.n_dpn);
+        EditText txtnblkng = (EditText) findViewById(R.id.n_blkng);
+        EditText txtnim = (EditText) findViewById(R.id.NIM);
+        EditText txtthn_angkatan = (EditText) findViewById(R.id.thn_angkatan);
+        EditText txtno_mkn = (EditText) findViewById(R.id.no_mkn);
+        EditText txtpwd = (EditText) findViewById(R.id.pwd);
+        EditText txtkon_pwd = (EditText) findViewById(R.id.kon_pwd);
+        Button btnDaftar = (Button) findViewById(R.id.btn_dft);
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validateForm();
+                if(TextUtils.isEmpty(txtn_dpn.getText()) || TextUtils.isEmpty(txtnblkng.getText()) || TextUtils.isEmpty(txtnim.getText()) || TextUtils.isEmpty(txtthn_angkatan.getText())
+                || TextUtils.isEmpty(txtno_mkn.getText()) || TextUtils.isEmpty(txtpwd.getText()) || TextUtils.isEmpty(txtkon_pwd.getText())){
+//              Membuat alertdialog bahwa ada field yang kosong
+                    AlertDialog.Builder apakahKosong = new AlertDialog.Builder(registrasi.this);
+                    apakahKosong.setTitle("WARNING!");
+                    apakahKosong.setMessage("ADA FIELD YANG KOSONG!!!");
+                    apakahKosong.setCancelable(true);
+                    apakahKosong.setNeutralButton("OKE", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    AlertDialog kosong = apakahKosong.create();
+                    kosong.show();
+                } else if (!txtpwd.getText().toString().equals(txtkon_pwd.getText().toString())) {
+                    AlertDialog.Builder apakahsama = new AlertDialog.Builder(registrasi.this);
+                    apakahsama.setTitle("WARNING!");
+                    apakahsama.setMessage("Password dan Konfirmasi tidak sama!!!");
+                    apakahsama.setCancelable(true);
+                    apakahsama.setNeutralButton("OKE", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    AlertDialog sama = apakahsama.create();
+                    sama.show();
+                }
+                else {
+                    AlertDialog.Builder sukses = new AlertDialog.Builder(registrasi.this);
+                    sukses.setTitle("Sukses!");
+                    sukses.setMessage("Pendaftaran Kamu berhasil :o");
+                    sukses.setCancelable(true);
+                    sukses.setNeutralButton("OKE", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    AlertDialog Sukses = sukses.create();
+                    Sukses.show();
+                }
             }
+
         });
     }
-
-    private void validateForm() {
-        if (TextUtils.isEmpty(konPwdEditText.getText()) || TextUtils.isEmpty(nDpnEditText.getText()) ||
-                TextUtils.isEmpty(nBlkngEditText.getText()) || TextUtils.isEmpty(nIMEditText.getText()) ||
-                TextUtils.isEmpty(thnAngkatanEditText.getText()) || TextUtils.isEmpty(noMknEditText.getText()) ||
-                TextUtils.isEmpty(pwdEditText.getText())) {
-            // Salah satu atau lebih field kosong, tampilkan pesan kesalahan
-            Toast.makeText(registrasi.this, "Semua field harus diisi!", Toast.LENGTH_SHORT).show();
-        } else {
-            // Semua field diisi, lanjutkan proses registrasi
-            // Implementasikan logika registrasi di sini
-        }
-    }
 }
+
